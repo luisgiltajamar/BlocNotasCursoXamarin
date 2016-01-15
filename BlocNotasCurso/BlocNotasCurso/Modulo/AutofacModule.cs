0926c6585959f5f51161431390294bb79116f556
Luis Felipe Gil Lamaignere <luis.gil@toolents.com>
2016-01-15 10:53:05 +0100

    XD

---
+++ BlocNotasCurso/BlocNotasCurso/Modulo/AutofacModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using BlocNotasCurso.Factorias;
+using Xamarin.Forms;
+
+namespace BlocNotasCurso.Modulo
+{
+    public class AutofacModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ViewFactory>().
+                As<IViewFactory>().
+                SingleInstance();
+            builder.RegisterType<Navigator>().
+                As<INavigator>().SingleInstance();
+            builder.Register<INavigation>(
+                ctx=> App.Current.MainPage.Navigation).SingleInstance();
+
+
+        }
+    }
+}
---
+++ BlocNotasCurso/BlocNotasCurso/Modulo/BlocNotasModulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Autofac;
+using BlocNotasCurso.Service;
+using Xamarin.Forms;
+
+namespace BlocNotasCurso.Modulo
+{
+    public class BlocNotasModulo:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ServicioDatosImpl>()
+                .As<IServicioDatos>().SingleInstance();
+
+            builder.RegisterInstance<Func<Page>>(() =>
+            {
+                var masterP = App.Current.MainPage as MasterDetailPage;
+                var page = masterP != null ? masterP.Detail : App.Current.MainPage;
+                var navigation = page as IPageContainer<Page>;
+                return navigation != null ? navigation.CurrentPage : page;
+            })
+            ;
+
+        }
+    }
+}
